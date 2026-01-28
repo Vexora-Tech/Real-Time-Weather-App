a66292bb1c3d879f0a9ba30e7f23940ecd931dc6
vansh <vanshdhaka02@gmail.com>
2026-01-28 22:41:15 +0530

    Search Component done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
     <>
       <div className="flex flex-col min-h-screen justify-center items-center bg-linear-to-br from-blue-200 via-indigo-200 to-purple-200">
         <div className="bg-zinc-800/90 text-white rounded-lg shadow-lg p-8 max-w-md w-full">
-          <h1 className="text-3xl font-bold text-center">Weather App</h1>
+          <h1 className="text-3xl font-bold text-center mb-6">Weather App</h1>
+          <SearchBar />
         </div>
       </div>
     </>
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+function SearchBar() {
+  const [city, setCity] = useState("");
+  return (
+    <form className="flex">
+      <input
+        type="text"
+        placeholder="Enter City Name"
+        value={city}
+        onChange={(e) => {
+          setCity(e.target.value);
+        }}
+        className="flex-1 p-2 border border-zinc-500 rounded-l-lg outline-none border-r-0"
+      />
+      <button
+        className="bg-zinc-500 p-2 hover:bg-zinc-600 cursor-pointer border border-l-0 rounded-r-lg"
+        type="submit"
+      >
+        Search
+      </button>
+    </form>
+  );
+}
+
+export default SearchBar;
