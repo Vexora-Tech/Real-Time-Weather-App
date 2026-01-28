26ba40eda61e030b35c73b235865124768323ad5
vansh <vanshdhaka02@gmail.com>
2026-01-28 22:24:02 +0530

    main theme done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <h1>hello</h1>
+      <div className="flex flex-col min-h-screen justify-center items-center bg-linear-to-br from-blue-200 via-indigo-200 to-purple-200">
+        <div className="bg-zinc-800/90 text-white rounded-lg shadow-lg p-8 max-w-md w-full">
+          <h1 className="text-3xl font-bold text-center">Weather App</h1>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
